e3c3bafb3f102fb2fc051b142dcc8e4b15857f0b
hsalgi100 <helen.salgi@gmail.com>
2017-09-23 17:40:06 -0500

    Create voting.py

---
+++ voting.py
@@ -0,0 +1,12 @@
+# Population voting
+
+"""
+Variables: number_of_candidates -> int
+           list_of_candidates   -> list of ints
+           candidate_n          -> int
+               ->  n will change for which candidate it is
+           population_number    -> int
+           person               -> key in a dict (int)
+           voting_result        -> value in a dict (int)
+           winner               -> int
+"""
